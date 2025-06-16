ac31032936240e7655162ecd4251b148a4c4a1a9
andrzejwozny <xemsss@gmail.com>
2025-06-16 20:44:55 +0300

    more lemon

--- app/src/main/java/com/xemsssa/lemonadeapp/MainActivity.kt
+++ app/src/main/java/com/xemsssa/lemonadeapp/MainActivity.kt
@@ -12,11 +12,16 @@ import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.wrapContentSize
+import androidx.compose.material3.Button
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Surface
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
+import androidx.compose.runtime.setValue
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.res.painterResource
@@ -42,33 +47,68 @@ class MainActivity : ComponentActivity() {
 
 @Composable
 fun LemonTree() {
+
+    var screen by remember { mutableStateOf(1) }
     Surface (
         color = MaterialTheme.colorScheme.background
     ){
-        Column (
-            modifier = Modifier.fillMaxSize().wrapContentSize(Alignment.Center),
-            horizontalAlignment = Alignment.CenterHorizontally
-        ){
-            Image(
-                painter = painterResource(R.drawable.lemon_tree),
-                contentDescription = null,
+
+        when(screen) {
+            1 -> {
+                Column (
+                    modifier = Modifier.fillMaxSize().wrapContentSize(Alignment.Center),
+                    horizontalAlignment = Alignment.CenterHorizontally
+                ){
+                    Button(
+                        onClick = { screen = 2 }
+                    ) {
+                        Image(
+                            painter = painterResource(R.drawable.lemon_tree),
+                            contentDescription = null,
 //                modifier = Modifier.clickable (
 //                    onClick = click
 //                ) {
 //                }
-            )
+                        )
+                    }
 
-            Spacer(
-                modifier = Modifier.height(24.dp)
-            )
+                    Spacer(
+                        modifier = Modifier.height(24.dp)
+                    )
 
-            Text(
-                text = stringResource(R.string.tap_lemon_tree),
-                fontSize = 18.sp
-            )
+                    Text(
+                        text = stringResource(R.string.tap_lemon_tree),
+                        fontSize = 18.sp
+                    )
+                }
+            }
+            2 -> {
+                Column (
+                    modifier = Modifier.fillMaxSize().wrapContentSize(Alignment.Center),
+                    horizontalAlignment = Alignment.CenterHorizontally
+                ){
+                    Button(
+                        onClick = { screen = 3 }
+                    ) {
+                        Image(
+                            painter = painterResource(R.drawable.lemon_squeeze),
+                            contentDescription = null,
+                        )
+                    }
+
+                    Spacer(
+                        modifier = Modifier.height(24.dp)
+                    )
+
+                    Text(
+                        text = stringResource(R.string.tapping),
+                        fontSize = 18.sp
+                    )
+                }
+            }
         }
-    }
 
+    }
 }
 
 fun click() {
